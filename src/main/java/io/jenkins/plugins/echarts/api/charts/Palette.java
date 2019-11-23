c5330fbc783bdf9a04759c0a293c791d91ce019f
Ulli Hafner <ullrich.hafner@gmail.com>
2019-11-23 23:45:37 +0100

    Incrementalify plugin deployments.

--- src/main/java/io/jenkins/plugins/echarts/api/charts/Palette.java
+++ src/main/java/io/jenkins/plugins/echarts/api/charts/Palette.java
@@ -1,21 +1,21 @@
 package io.jenkins.plugins.echarts.api.charts;
 
 /**
- * Color palette for the static analysis tools.
+ * Color palette for charts.
  *
  * @author Ullrich Hafner
  */
 public enum Palette {
-    RED("#EF9A9A", "#FFCDD2"),
-    PURPLE("#CE93D8", "#E1BEE7"),
-    INDIGO("#9FA8DA", "#C5CAE9"),
-    TEAL("#80CBC4", "#B2DFDB"),
-    GREEN("#A5D6A7", "#C8E6C9"),
-    LIME("#E6EE9C", "#F0F4C3"),
     YELLOW("#FFF59D", "#FFF9C4"),
+    LIME("#E6EE9C", "#F0F4C3"),
+    GREEN("#A5D6A7", "#C8E6C9"),
+    BLUE("#90CAF9", "#90CAF9"),
+    TEAL("#80CBC4", "#B2DFDB"),
     ORANGE("#FFCC80", "#FFE0B2"),
-    BROWN("#BCAAA4", "#D7CCC8"),
-    BLUE("#90CAF9", "#90CAF9");
+    INDIGO("#9FA8DA", "#C5CAE9"),
+    PURPLE("#CE93D8", "#E1BEE7"),
+    RED("#EF9A9A", "#FFCDD2"),
+    BROWN("#BCAAA4", "#D7CCC8");
 
     private final String normal;
     private final String hover;
