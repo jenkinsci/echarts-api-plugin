5938f48b98535a05635f8fe9f1bacb171021df08
Ulli Hafner <ullrich.hafner@gmail.com>
2019-12-18 22:53:09 +0100

    Move all Java code to echarts-build-trends library.

--- src/main/java/io/jenkins/plugins/echarts/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides server side logic and JSON models for the ECharts JS library.
- */
-@DefaultAnnotation(NonNull.class)
-package io.jenkins.plugins.echarts;
-
-import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
-import edu.umd.cs.findbugs.annotations.NonNull;
