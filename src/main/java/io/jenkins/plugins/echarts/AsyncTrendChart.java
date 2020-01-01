305ec5749f358e66fcefc841868d12aae8336bac
Ulli Hafner <ullrich.hafner@gmail.com>
2020-01-01 22:41:57 +0100

    Provide base classes and a tag for trend charts.

---
+++ src/main/java/io/jenkins/plugins/echarts/AsyncTrendChart.java
@@ -0,0 +1,36 @@
+package io.jenkins.plugins.echarts;
+
+import edu.hm.hafner.echarts.LinesChartModel;
+
+import org.kohsuke.stapler.bind.JavaScriptMethod;
+
+/**
+ * Provides the trend chart for a job. The trend chart model will be rendered by ECharts: the model of the chart will be
+ * obtained using an asynchronous Ajax call.
+ *
+ * @author Ullrich Hafner
+ */
+public interface AsyncTrendChart {
+    /**
+     * Returns the UI model for an ECharts chart that shows the build trend. The model has to be a JSON representation
+     * of a corresponding {@link LinesChartModel} instance, that will be inserted into the client side model of the
+     * corresponding ECharts instance.
+     *
+     * @return the UI model as JSON
+     * @see LinesChartModel for details on how to construct such a model
+     * @see AsyncTrendJobAction for an example on how to provide the JSON representation
+     */
+    @JavaScriptMethod
+    @SuppressWarnings("unused")
+    // Called by jelly view
+    String getBuildTrendModel();
+
+    /**
+     * Returns whether the trend report should be shown.
+     *
+     * @return {@code true} if the trend should be shown, {@code false} otherwise
+     */
+    @SuppressWarnings("unused")
+    // Called by jelly view
+    boolean isTrendVisible();
+}
---
+++ src/main/java/io/jenkins/plugins/echarts/AsyncTrendJobAction.java
@@ -0,0 +1,80 @@
+package io.jenkins.plugins.echarts;
+
+import java.util.Iterator;
+
+import edu.hm.hafner.echarts.BuildResult;
+import edu.hm.hafner.echarts.JacksonFacade;
+import edu.hm.hafner.echarts.LinesChartModel;
+
+import org.kohsuke.stapler.bind.JavaScriptMethod;
+import hudson.model.Job;
+
+import io.jenkins.plugins.util.BuildAction;
+import io.jenkins.plugins.util.JobAction;
+
+/**
+ * A job action displays a link on the side panel of a job that refers to the last build that contains results (i.e. a
+ * {@link BuildAction} with a corresponding result). This action also is responsible to render the historical trend via
+ * its associated 'floatingBox.jelly' view. The trend chart will be rendered by ECharts: the model of the chart will be
+ * obtained using an asynchronous Ajax call.
+ *
+ * @param <T>
+ *         type of the results
+ *
+ * @author Ullrich Hafner
+ */
+public abstract class AsyncTrendJobAction<T extends BuildAction<?>> extends JobAction<T> implements AsyncTrendChart {
+    /**
+     * Creates a new instance of {@link AsyncTrendJobAction}.
+     *
+     * @param owner
+     *         the job that owns this action
+     * @param buildActionClass
+     *         the type of the action to find
+     */
+    protected AsyncTrendJobAction(final Job<?, ?> owner, final Class<T> buildActionClass) {
+        super(owner, buildActionClass);
+    }
+
+    @Override
+    @JavaScriptMethod
+    @SuppressWarnings("unused") // Called by jelly view
+    public String getBuildTrendModel() {
+        return new JacksonFacade().toJson(createChartModel());
+    }
+
+    /**
+     * This default implementation checks whether there are at least two build results available. Override if this
+     * behavior does not make sense in your job action.
+     *
+     * @return {@code true} if there are at least two results, {@code false} otherwise
+     */
+    @Override
+    @SuppressWarnings("unused") // Called by jelly view
+    public boolean isTrendVisible() {
+        return hasAtLeastTwoResults();
+    }
+
+    private boolean hasAtLeastTwoResults() {
+        Iterable<? extends BuildResult<T>> results = createBuildHistory();
+        Iterator<? extends BuildResult<T>> iterator = results.iterator();
+
+        if (iterator.hasNext()) {
+            iterator.next();
+        }
+        return iterator.hasNext();
+    }
+
+    /**
+     * Creates the model of the trend chart. The returned model will be converted to JSON and inserted into the client
+     * side model of the corresponding ECharts instance.
+     *
+     * @return the model of the trend chart
+     * @see LinesChartModel for details on how to construct such a model
+     */
+    protected abstract LinesChartModel createChartModel();
+
+    protected Iterable<? extends BuildResult<T>> createBuildHistory() {
+        return () -> new BuildActionIterator<>(getBuildActionClass(), getLatestAction());
+    }
+}
