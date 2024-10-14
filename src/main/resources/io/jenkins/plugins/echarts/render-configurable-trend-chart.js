d542ca5d130a6ea22b7843dc07573e5ccbaac7be
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-14 19:46:03 +0300

    Make sure correct `trendProxy` is passed

--- src/main/resources/io/jenkins/plugins/echarts/render-configurable-trend-chart.js
+++ src/main/resources/io/jenkins/plugins/echarts/render-configurable-trend-chart.js
@@ -3,9 +3,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
     dataHolders.forEach(dataHolder => {
         const chartId = dataHolder.getAttribute("data-chart-id");
-        const enableLinks = dataHolder.getAttribute("data-enable-links") || false;
+        const enableLinks = dataHolder.getAttribute("data-enable-links");
         const configurationId = dataHolder.getAttribute("data-configuration-id");
+        const generatedId = dataHolder.getAttribute("data-generated-id");
 
-        echartsJenkinsApi.renderConfigurableTrendChart(`${chartId}`, `${enableLinks}`, `${configurationId}`, trendProxy);
+        echartsJenkinsApi.renderConfigurableTrendChart(`${chartId}`, `${enableLinks}`, `${configurationId}`, window[`trendProxy${generatedId}`]);
     });
 });
